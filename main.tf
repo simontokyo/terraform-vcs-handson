518a59c56b45654f7ade3e0dc836ff96ee91b034
Simon Sasaki <simontokyo@users.noreply.github.com>
2025-09-26 14:47:36 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,6 +6,6 @@ resource "aws_instance" "allowed_example" {
   ami           = "ami-0c3fd0f5d33134a76" # Amazon Linux 2 (東京リージョン)
   instance_type = "t3.micro"
   tags = {
-    Name = "allowed-instance"
+    Name = "allowed-instance-2-hands-on"
   }
 }
